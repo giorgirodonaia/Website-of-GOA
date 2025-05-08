b63c5550ecf17b4ebe6f5234330976fb32e228e7
giorgirodonaia <giorgirodonaia20@gmail.com>
2025-05-08 10:23:21 -0700

    added image of hakathon section

---
+++ script.js
@@ -0,0 +1,13 @@
+const hakImg = [
+    "img1.jfif",
+    "img2.jfif",
+    "img3.jfif",
+    "img4.jfif",
+    "img5.jfif",
+    "img6.jfif",
+    "img7.jfif",
+    "img8.jfif",
+    "img9.jfif"
+]
+
+let index
