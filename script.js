7a4175b9565c89747ca8d6c857ecab2b50fc147d
giorgirodonaia <giorgirodonaia20@gmail.com>
2025-05-08 12:13:05 -0700

    creted section of hakathon

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 const hakImg = [
-    "img1.jfif",
-    "img2.jfif",
-    "img3.jfif",
-    "img4.jfif",
-    "img5.jfif",
-    "img6.jfif",
-    "img7.jfif",
-    "img8.jfif",
-    "img9.jfif"
+    "./ImagesHak/img1.jfif",
+    "./ImagesHak/img2.jfif",
+    "./ImagesHak/img3.jfif",
+    "./ImagesHak/img4.jfif",
+    "./ImagesHak/img5.jfif",
+    "./ImagesHak/img6.jfif",
+    "./ImagesHak/img7.jfif",
+    "./ImagesHak/img8.jfif",
+    "./ImagesHak/img9.jfif"
 ]
 
-let index
+let index = 0
+
+
+const imgSlide = document.getElementById("imgSlide");
+const backBtn = document.getElementById("backBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+
+nextBtn.addEventListener("click", () => {
+    index = (index + 1) % hakImg.length
+    imgSlide.src = hakImg[index]
+})
+
+backBtn.addEventListener("click", () => {
+    index = (index - 1 + hakImg.length) % hakImg.length
+    imgSlide.src = hakImg[index]
+})
+
+
+
+
+
+const hakAnchor = document.getElementById("hakAnchor");
+    let  isVisible = true
+
+hakAnchor.addEventListener("click", () => {
+    const hakParentDiv = document.getElementById("hacathon_parent_div")
+
+    if(isVisible){
+        hakParentDiv.style.display = "block"
+        // isVisible = !isVisible
+    }else {
+        hakParentDiv.style.display = "none"
+        
+    }
+    isVisible= !isVisible
+})
