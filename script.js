839caafbbfa33df387d4c203b754432ffa0650ea
giorgirodonaia <giorgirodonaia20@gmail.com>
2025-05-09 12:33:22 -0700

    added new section(statistics)

--- script.js
+++ script.js
@@ -40,10 +40,53 @@ hakAnchor.addEventListener("click", () => {
 
     if(isVisible){
         hakParentDiv.style.display = "block"
-        // isVisible = !isVisible
     }else {
         hakParentDiv.style.display = "none"
         
     }
     isVisible= !isVisible
 })
+
+const academies = [
+    {
+        logo: "./408864179_342479481868687_2724919989645877908_n.jpg",
+        students: 1500,
+        color: "green"
+    },
+    {
+        logo: "./academysLogo/ITstep.png",
+        students: 700,
+        color: "cyan"
+    },
+    {
+        logo: "./academysLogo/Mziuri.png",
+        students: 1900,
+        color: "yellow"
+    },
+    {
+        logo: "./acdemysLogo/BitCamp.png",
+        students: 1300,
+        color: "red"
+    }
+] 
+
+function statisticsRender(arr){
+    let main = document.getElementById("academiesStatistics")
+
+    for(let i = 0; i < academies.length; i++) {
+        main.append(finalStatistics(arr[i]))
+    }
+}
+
+statisticsRender(academies)
+
+const statisticsBtn = document.getElementById("statisticsBtn");
+
+ function finalStatistics(academy){
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    let imgRes = img.src = academy.logo
+
+    
+ } 
+    
